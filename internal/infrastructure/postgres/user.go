c02587a25d3af4d5c3f66e2abac7b20b4dd103a0
tyomaaans <darmantosastro89@gmail.com>
2026-08-24 01:22:20 +0700

    Update User

--- internal/infrastructure/postgres/user.go
+++ internal/infrastructure/postgres/user.go
@@ -4,9 +4,9 @@ import "time"
 
 type UserStorage struct {
 	ID         string    `gorm:"primaryKey"`
-	Name       string    `gorm:"varchar(255);not null"`
-	Email      string    `gorm:"varchar(255);uniqueIndex;not null"`
-	Password   string    `gorm:"varchar(255);not null"`
+	Name       string    `gorm:"type:varchar(255);not null"`
+	Email      string    `gorm:"type:varchar(255);uniqueIndex;not null"`
+	Password   string    `gorm:"type:varchar(255);not null"`
 	RememberMe bool      `gorm:"not null;default:false"`
 	CreatedAt  time.Time `gorn:"not null"`
 	UpdatedAt  time.Time `gorm:"not null"`
